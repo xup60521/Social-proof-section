7318d83a3f5348d4d2845af36d38bcd6ec163b20
xup60521 <84887662+xup60521@users.noreply.github.com>
2024-08-25 13:36:46 +0800

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ export default {
     theme: {
         extend: {
             colors: {
-
+                c_Very_Dark_Magenta: "hsl(300, 43%, 22%)",
+                c_Soft_Pink: "hsl(333, 80%, 67%)",
+                c_Dark_Grayish_Magenta: "hsl(303, 10%, 53%)",
+                c_Light_Grayish_Magenta: "hsl(300, 24%, 96%)",
             },
             fontFamily: {
-
+                league_spartan: ["League Spartan", "sans-serif"]
             }
         },
     },
